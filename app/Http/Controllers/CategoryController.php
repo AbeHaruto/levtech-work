1cf2959f49a76510f5dd070570ef17f173b5f695
AbeHaruto <haru.dolphins6@gmail.com>
2022-11-16 19:37:50 +0900

    カテゴリー

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
+use App\Models\Post;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    /*
+     * Post一覧を表示する
+     *
+     * @param Post Postモデル
+     * @return array Postモデルリスト
+     */
+    public function index(Category $category)
+    {
+        return view('categories.index')->with(['posts' => $category->getByCategory()]);
+    }
+}
